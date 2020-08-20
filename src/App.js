7cf69ceb4ec99e455c117c5c5cefd9ff5ebcd16f
moshtaba <x@Air.local>
2020-08-21 02:47:50 +0430

    13

--- src/App.js
+++ src/App.js
@@ -5,42 +5,42 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
+      <AddToHomeScreen
+        appId="Add-to-Homescreen React Live Demo"
+        startAutomatically={true}
+        startDelay={0}
+        lifespan={30}
+        skipFirstVisit={true}
+        displayPace={0}
+        customPromptContent={{
+          cancelMsg: "",
+          installMsg: "Install",
+          guidanceCancelMsg: "",
+        }}
+        customPromptElements={{
+          container: "athContainer",
+          containerAddOns: "",
+          banner: "athBanner",
+          logoCell: "athLogoCell",
+          logoCellAddOns: "athContentCell",
+          logo: "athLogo",
+          titleCell: "athTitleCell",
+          titleCellAddOns: "athContentCell",
+          title: "athTitle",
+          cancelButtonCell: "athCancelButtonCell",
+          cancelButtonCellAddOns: "athButtonCell",
+          cancelButton: "athCancelButton",
+          installButtonCell: "athInstallButtonCell",
+          installButtonCellAddOns: "athButtonCell",
+          installButton: "athInstallButton",
+          installButtonAddOns: "button",
+          guidance: "athGuidance",
+          guidanceImageCell: "athGuidanceImageCell",
+          guidanceImageCellAddOns: "",
+          guidanceCancelButton: "athGuidanceCancelButton",
+        }}
+      />
       <header className="App-header">
-        <AddToHomeScreen
-          appId="Add-to-Homescreen React Live Demo"
-          startAutomatically={true}
-          startDelay={0}
-          lifespan={30}
-          skipFirstVisit={true}
-          displayPace={0}
-          customPromptContent={{
-            cancelMsg: "",
-            installMsg: "Install",
-            guidanceCancelMsg: "",
-          }}
-          customPromptElements={{
-            container: "athContainer",
-            containerAddOns: "",
-            banner: "athBanner",
-            logoCell: "athLogoCell",
-            logoCellAddOns: "athContentCell",
-            logo: "athLogo",
-            titleCell: "athTitleCell",
-            titleCellAddOns: "athContentCell",
-            title: "athTitle",
-            cancelButtonCell: "athCancelButtonCell",
-            cancelButtonCellAddOns: "athButtonCell",
-            cancelButton: "athCancelButton",
-            installButtonCell: "athInstallButtonCell",
-            installButtonCellAddOns: "athButtonCell",
-            installButton: "athInstallButton",
-            installButtonAddOns: "button",
-            guidance: "athGuidance",
-            guidanceImageCell: "athGuidanceImageCell",
-            guidanceImageCellAddOns: "",
-            guidanceCancelButton: "athGuidanceCancelButton",
-          }}
-        />
         <b>PWA Reactjs</b>
       </header>
     </div>
