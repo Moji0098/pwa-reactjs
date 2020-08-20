d814f431189615292ac002c744681995e774b1b1
moshtaba <x@Air.local>
2020-08-21 01:45:37 +0430

    add 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <AddToHomeScreen />
+        <AddToHomeScreen
+          appId="Add-to-Homescreen React Modified Styling Example"
+          customPromptContent={{
+            cancelMsg: "",
+            installMsg: "Install",
+            guidanceCancelMsg: "",
+          }}
+          customPromptElements={{
+            container: "athContainer",
+            containerAddOns: "",
+            banner: "athBanner",
+            logoCell: "athLogoCell",
+            logoCellAddOns: "athContentCell",
+            logo: "athLogo",
+            titleCell: "athTitleCell",
+            titleCellAddOns: "athContentCell",
+            title: "athTitle",
+            cancelButtonCell: "athCancelButtonCell",
+            cancelButtonCellAddOns: "athButtonCell",
+            cancelButton: "athCancelButton",
+            installButtonCell: "athInstallButtonCell",
+            installButtonCellAddOns: "athButtonCell",
+            installButton: "athInstallButton",
+            installButtonAddOns: "button",
+            guidance: "athGuidance",
+            guidanceImageCell: "athGuidanceImageCell",
+            guidanceImageCellAddOns: "",
+            guidanceCancelButton: "athGuidanceCancelButton",
+          }}
+        />
         <b>PWA Reactjs</b>
       </header>
     </div>
